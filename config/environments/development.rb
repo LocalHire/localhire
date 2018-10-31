7dfcd8b3fee60a1bc97344a261be7a046b0f5c2c
Sherin <samuel.sherin2@gmail.com>
2018-10-31 17:06:16 +1100

    removed keys

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,12 +62,11 @@ Rails.application.configure do
   #details from Domain details in mailgun
 
 config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-address:              'smtp.mailgun.org',
-port:                 587,
-domain:               'sandbox78d2a7ae0e674371bca4da4f09177c3d.mailgun.org',
-user_name:            'postmaster@sandbox78d2a7ae0e674371bca4da4f09177c3d.mailgun.org',
-password:             'be96d9e54b2e8d6ff11bce2969c56bdf-4836d8f5-18d08496',
-authentication:       'plain',
-enable_starttls_auto: true }
-end
+  config.action_mailer.smtp_settings = {
+  address:              ENV['MAILER_ADDRESS'],
+  port:                 587,
+  domain:               ENV['MAILER_DOMAIN'],
+  user_name:            ENV['MAILER_USER_NAME'],
+  password:             ENV['MAILER_PASSWORD'],
+  authentication:       'plain',
+  enable_starttls_auto: true }
