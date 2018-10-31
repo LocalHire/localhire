17423fbfe988f4aeb1fa187e73f9e4de6889b03d
Sherin <samuel.sherin2@gmail.com>
2018-11-01 05:12:52 +1100

    removed mailer details

--- app/mailers/localhire_mailer.rb
+++ app/mailers/localhire_mailer.rb
@@ -10,16 +10,4 @@ end
 #when testing, update these files with your details
 
 #mailers/localhire_mailer (this one)
-#config/environments/development :
-
-            ##details from Domain details in mailgun
-
-            # config.action_mailer.delivery_method = :smtp
-            #   config.action_mailer.smtp_settings = {
-            #   address:              'smtp.mailgun.org',
-            #   port:                 587,
-            #   domain:               'sandbox78d2a7ae0e674371bca4da4f09177c3d.mailgun.org',
-            #   user_name:            'postmaster@sandbox78d2a7ae0e674371bca4da4f09177c3d.mailgun.org',
-            #   password:             'be96d9e54b2e8d6ff11bce2969c56bdf-4836d8f5-18d08496',
-            #   authentication:       'plain',
-            #   enable_starttls_auto: true }
+#config/environments/development : update details from Domain details in mailgun
