b65976b7ae24c7b36e4c94c2d984cf1a835fff45
guymcmanusgithub <guy@guymcmanus.com>
2018-11-05 11:28:10 +1100

    fixed up create product so now redirects to show, fixed up created page so now durations hide when not clicked upon

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -62,7 +62,7 @@ class ItemsController < ApplicationController
     respond_to do |format|
       if @item.save
 
-        format.html { redirect_to edit_item_path(@item), notice: 'Item was successfully created.' }
+        format.html { redirect_to item_path(@item), notice: 'Item was successfully created.' }
         # format.html { redirect_to @item, notice: 'Item was successfully created.' }
         format.json { render :show, status: :created, location: @item }
       else
