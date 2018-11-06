8e221de736d6373d6bbb01dde4dd6346af5b04c9
guymcmanusgithub <guy@guymcmanus.com>
2018-11-07 10:03:53 +1100

    took out geocoder codes

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -7,7 +7,7 @@ Geocoder.configure(
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: "AIzaSyCV4f9NO5g6qwbT_48ZnONFUEerHqlMJWc",             # API key for geocoding service
+  api_key: "",             # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
